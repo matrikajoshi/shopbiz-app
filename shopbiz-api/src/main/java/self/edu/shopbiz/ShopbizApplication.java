969301843c66548e47e9f09e6fde070ab036a489
mpjoshi <matrica.joshi@gmail.com>
2020-03-01 19:37:09 -0500

    Adding back micro-services dependencies

--- shopbiz-api/src/main/java/self/edu/shopbiz/ShopbizApplication.java
+++ shopbiz-api/src/main/java/self/edu/shopbiz/ShopbizApplication.java
@@ -3,12 +3,19 @@ package self.edu.shopbiz;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-
+@EnableEurekaClient
+@EnableFeignClients
+@EnableHystrix
+@EnableHystrixDashboard
 public class ShopbizApplication {
 
 	public static void main(String[] args) {
--- shopbiz-api/src/main/java/self/edu/shopbiz/restclients/CouponClient.java
+++ shopbiz-api/src/main/java/self/edu/shopbiz/restclients/CouponClient.java
@@ -1,14 +1,17 @@
 package self.edu.shopbiz.restclients;
 
 
-
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import self.edu.shopbiz.dto.Coupon;
 
 import java.util.List;
 
-//@FeignClient("zuul-api-gateway")
+@FeignClient("zuul-api-gateway")
+//@FeignClient("COUPON-SERVICE")
+//@RibbonClient("COUPON-SERVICE")
 public interface CouponClient {
 
     // TO DO - cover with integration test, no unit testing
